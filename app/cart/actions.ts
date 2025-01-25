d01d14f6bad4190c79be859a41570d101b57a025
Adekunle Adeoya <adeoyaadekunle@gmail.com>
2025-01-24 21:10:39 -0330

    cart

---
+++ app/cart/actions.ts
@@ -0,0 +1,37 @@
+'use server'
+
+import { getCart, createCart } from '@/lib/db/cart'
+import prisma from '@/lib/db/prisma'
+import { revalidatePath } from 'next/cache'
+
+async function setProductQuantity(productId: string, quantity: number) {
+    const cart = (await getCart()) ?? (await createCart())
+
+    const articleInCart = cart?.items.find(item => item.productId === productId)
+
+    if (quantity === 0) {
+        if (articleInCart) {
+            await prisma.cartItem.delete({
+                where: { id: articleInCart.id },
+            })
+        }
+    } else {
+        if (articleInCart) {
+            await prisma.cartItem.update({
+                where: { id: articleInCart.id },
+                data: { quantity },
+            })
+        } else {
+            await prisma.cartItem.create({
+                data: {
+                    cartId: cart?.id,
+                    productId,
+                    quantity: 1,
+                },
+            })
+        }
+        revalidatePath('/products/[id]')
+    }
+}
+
+export default setProductQuantity
