ce4cfe4294d0bc426ee38cbcb2048817b99836f7
Adekunle Adeoya <adeoyaadekunle@gmail.com>
2025-01-25 00:07:19 -0330

    tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,34 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+      './src/**/*.{html,js}',
+      './pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './components/**/*.{js,ts,jsx,tsx,mdx}',
+      './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+      extend: {},
   },
-  plugins: [],
-} satisfies Config;
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  plugins: [require('daisyui')],
+  daisyui: {
+      themes: [
+          {
+              lightTheme: {
+                  primary: '#f4aa3a',
+                  secondary: '#f4f4a1',
+                  accent: '#1be885',
+                  neutral: '#272136',
+                  'base-100': '#ffffff',
+                  info: '#778ad4',
+                  success: '#23b893',
+                  warning: '#f79926',
+                  error: '#ea535a',
+                  body: {
+                      'background-color': '#e3e6e6',
+                  },
+              },
+          },
+      ],
+  },
+}
