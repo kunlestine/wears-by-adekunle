41b69b74552ed5b0c19e8134fd4ec67d119db432
Adekunle Adeoya <adeoyaadekunle@gmail.com>
2025-01-30 18:54:16 -0330

    updated

--- lib/password.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const bcrypt = require('bcrypt')
-
-export default function saltAndHashPassword(unHashedPass: string) {
-    return bcrypt.hash(unHashedPass, 10).then(function (hash: string) {
-        return hash
-    })
-}
