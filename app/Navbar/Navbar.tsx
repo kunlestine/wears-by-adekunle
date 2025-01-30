9102c674b5306c4203e620aec44350e2557d265d
Adekunle Adeoya <adeoyaadekunle@gmail.com>
2025-01-30 18:40:40 -0330

    updated

--- app/Navbar/Navbar.tsx
+++ app/Navbar/Navbar.tsx
@@ -1,17 +1,16 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import logo from '@/app/assets/WhatsApp Image 2024-12-06 at 11.34.07 AM.jpeg'
+import logo from '@/public/assets/adekunle_logo.jpeg'
 import { redirect } from 'next/navigation'
 import { getCart } from '@/lib/db/cart'
 import ShoppingCartButton from './ShoppingCartButton'
 import UserMenuButton from './UserMenuButton'
-import { useSession } from "next-auth/react"
+import { auth } from "@/auth";
+
 
 async function searchProducts(formData: FormData) {
     'use server'
-    
-
     const searchQuery = formData.get('searchQuery')?.toString()
 
     if (searchQuery) {
@@ -19,45 +18,44 @@ async function searchProducts(formData: FormData) {
     }
 }
 
-export default async function Navbar() {
-    const { data: session, status} = useSession()
-    // const session = await getServerSession(authOptions)
-    if (status === "authenticated") {
-        return <p>Signed in as {session.user.email}</p>
-      }
+export default async function Navbar () {
+    
+    const session = await auth();
+    const user = session?.user?.email
     const cart = await getCart()
 
     return (
-        <div className="bg-base-100">
+        <div className="bg-base-100 h-28">
             <div className="navbar m-auto max-w-7xl flex-col gap-2 sm:flex-row">
                 <div className="flex-1">
                     <Link
                         href="/"
-                        className="btn btn-ghost text-xl normal-case"
+                        className="btn btn-ghost text-xl normal-case h-fit"
                     >
                         <Image
                             src={logo}
                             alt="Wears_by_Adekunle logo"
-                            height={40}
-                            width={40}
+                            height={10}
+                            width={100}
                         />
-                        KC
+
                     </Link>
                 </div>
-                <div className="flex-none gap-2">
-                    <form action={searchProducts}>
-                        <div className="form-control">
-                            <input
-                                name="SearchQuery"
-                                placeholder="Search"
-                                className="input input-bordered w-full min-w-[100px]"
-                            />
-                        </div>
-                    </form>
-                    <ShoppingCartButton cart={cart} />
-                    {/* <UserMenuButton session={session} /> */}
-                </div>
+            <div className="flex-none gap-2">
+                <form action={searchProducts}>
+                    <div className="form-control">
+                        <input
+                            name="SearchQuery"
+                            placeholder="Search"
+                            className="input input-bordered w-full min-w-[100px]"
+                        />
+                    </div>
+                </form>
+                {user && <p className='flex px-2'>Welcome <span className='text-blue-700'>{session?.user?.name}!</span></p>}
+                <ShoppingCartButton cart={cart} />
+                <UserMenuButton session={session} />    
             </div>
         </div>
+    </div>
     )
 }
